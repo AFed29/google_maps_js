a6a118cdfec872d75d08fcb4e4dcf0454571a308
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-04-18 00:07:02 +0100

    Added geolocation functionality to map

--- public/app.js
+++ public/app.js
@@ -13,9 +13,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
   mainMap.addClickListener();
 
-  const moveToPerthButton = document.querySelector('#take_to_perth')
+  const moveToPerthButton = document.querySelector('#take_to_perth');
   moveToPerthButton.addEventListener('click', function () {
-    console.log('click');
     mainMap.googleMap.setCenter({lat: -31.950527, lng: 115.860457});
   });
+
+  const whereAmIButton = document.querySelector('#where_am_i');
+  whereAmIButton.addEventListener('click', function () {
+    console.log('click');
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log(position.coords.latitude, position.coords.longitude);
+      mainMap.googleMap.setCenter({lat: position.coords.latitude, lng: position.coords.longitude});
+    });
+  });
+
 });
